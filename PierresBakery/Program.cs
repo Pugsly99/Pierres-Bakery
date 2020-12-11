4054dd306b7a76cf25a7361b25f7df16406e9e49
Pugsly99 <rylandadams@yahoo.com>
2020-12-11 11:48:36 -0800

    UI finished test 1

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -1,3 +1,4 @@
+using PierresBakery;
 using System;
 
 namespace PierresBakery.Models
@@ -11,18 +12,39 @@ namespace PierresBakery.Models
       Console.WriteLine("We currently offer Pastrys and Bread!");
       Console.WriteLine("Here are our deals currently:");
       Console.WriteLine("Bread - Buy 2, get 1 free. A single loaf costs $5.");
-      Console.WriteLine("Pastry - Buy 1 for $2 or 3 for $5.");
+      Console.WriteLine("Bread Received: " + breadAmount);
       Console.WriteLine("--------------------------------------------------");
 
       Console.WriteLine("How many Pastry's would you like?");
-      int pastryResponse = Console.ReadLine();
+      int pastryResponse = int.Parse(Console.ReadLine());
       int pastryPrice = 0;
       int pastryAmount = 0;
 
       Console.WriteLine("How many loaves of Bread would you like?");
-      int breadResponse = Console.ReadLine();
+      int breadResponse = int.Parse(Console.ReadLine());
       int breadPrice = 0;
       int breadAmount = 0;
+
+      Bakery newBakery = new Bakery();
+
+      newBakery.Pastry(pastryResponse);
+      pastryPrice = newBakery.PastryPrice;
+      pastryAmount = newBakery.PastryAmount;
+
+      newBakery.Bread(breadResponse);
+      breadPrice = newBakery.BreadPrice;
+      breadAmount = newBakery.BreadAmount;
+
+      Console.WriteLine("Pastry Price: " + pastryPrice + "$");
+      Console.WriteLine("Pastry Received: " + pastryAmount);
+
+      Console.WriteLine("Bread Price: " + breadPrice + "$");
+      Console.WriteLine("Bread Received: " + breadAmount);
+
+      int total = breadPrice + pastryPrice;
+      Console.WriteLine("Your total today will be: " + total + "$");
+
+
 
     }
   }
